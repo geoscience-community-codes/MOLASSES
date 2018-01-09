3de8c52c338bbc4fff17f3f3c590d59b0cddceea
ljc-geo <lconnor@usf.edu>
2018-01-09 14:58:29 -0500

    Update run_E-dip.pl

--- run_code_here/run_E-dip.pl
+++ run_code_here/run_E-dip.pl
@@ -1,4 +1,5 @@
-
+# File (path) names can be changed to satisfy the user
+$exe = "../bin/molasses.ljc";
 $config_file = "config.E-dip";
 $plot_file = "plot.gmt.pl";
 $plot_conf = "plot_conf.E-dip";
@@ -6,7 +7,7 @@ $plot_conf = "plot_conf.E-dip";
 ##### STOP - Do not edit past this line ###########################
 system "date";
 print "Running molasses .....\n";
-system "nohup ../bin/molasses.ljc $config_file 0 1 2>./logfile >./output < /dev/null ";
+system "nohup $exe $config_file 0 1 2>./logfile >./output < /dev/null ";
 print "........finished. Plotting lavaflow.\n";
 system "perl $plot_file $plot_conf flow0";
 print "Done.\n";

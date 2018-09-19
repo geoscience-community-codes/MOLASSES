4f705ec8807071db519533b7899b2436094f6344
Laura Connor <lconnor@usf.edu>
2018-09-19 12:35:56 -0400

    Added license statement at top of file.

--- src/initflow_LJC.c
+++ src/initflow_LJC.c
@@ -1,3 +1,27 @@
+/*############################################################################
+# MOLASSES (MOdular LAva Simulation Software for the Earth Sciences) 
+# The MOLASSES model relies on a cellular automata algorithm to 
+# estimate the area inundated by lava flows.MOLASSES 
+#
+#    Copyright (C) 2015-2020  
+#    Laura Connor (lconnor@usf.edu)
+#    Jacob Richardson 
+#    Charles Connor
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###########################################################################*/    
+
 #include "include/prototypes_LJC.h"
 
 /**************************************************

21d0bf2bfcc65442b85d8e51244f034e0fbc3cde
Laura Connor <lconnor@usf.edu>
2022-11-07 15:43:52 -0500

    Add GNU v3 license lines.

--- src/aoi.c
+++ src/aoi.c
@@ -1,4 +1,28 @@
-/* AOI.c */
+/*############################################################################
+# MOLASSES (MOdular LAva Simulation Software for the Earth Sciences) 
+# The MOLASSES model relies on a cellular automata algorithm to 
+# estimate the area inundated by lava flows.
+#
+#    Copyright (C) 2015-2021  
+#    Laura Connor (lconnor@usf.edu)
+#    Jacob Richardson 
+#    Charles Connor
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###########################################################################*/ 
+
+/* This file is AOI.c */
 
 #include "include/prototypes_LJC3.h"
 #define DIAGONAL_DISTANCE(x,y) (sqrt(((x)*(x))+((y)*(y))))
